d88e9af51c539407132c4521b7538987287e90e9
unknown <chotiwich6793@gmail.com>
2020-09-06 13:47:24 +0700

    add info

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,15 @@
 def validate_name(name):
+    blacklist = ["0", "1", "2", "3", "4", "5",
+                 "6", "7", "8", "9", "!", "@", "#", "$", ' ', ""]
+
+    if name == "":
+        return False
+
+    for charactor in name:
+
+        if charactor in blacklist:
+            return False
+
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,25 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("ดราก้อน"))
+
+    def test_validate_name_with_valid_input_int(self):
+        self.assertEqual(False, validate_name("1234"))
+
+    def test_validate_name_with_valid_input_contained_string_of_int(self):
+        self.assertEqual(False, validate_name("ดราก้อนDra123"))
+
+    def test_validate_name_with_valid_input_contained_string_of_special_charactor(self):
+        self.assertEqual(False, validate_name("Pu!"))
+
+    def test_validate_name_with_valid_input_contained_string_of_special_charactor_more_one(self):
+        self.assertEqual(False, validate_name("$หิว!เนี้ย#"))
+
+    def test_validate_name_with_valid_input_contained_not_blank(self):
+        self.assertEqual(False, validate_name(""))
+
+    def test_validate_name_with_valid_input_contained_empty_input(self):
+        self.assertEqual(False, validate_name("Puen Cho"))
 
 
 if __name__ == '__main__':
